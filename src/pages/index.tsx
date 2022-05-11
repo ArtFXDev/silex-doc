abda583fbb388b81011d56f4c91af6fcd771a3ef
Ayaee <afabre@artfx.fr>
2022-05-11 12:07:20 +0200

    testValid

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/user/prsentation">
+            to="/docs/user/presentation">
             Test Silex Doc
           </Link>
         </div>
