a9ef7ef77813b4253b70478cb04b5e2be205c1eb
OlivierArgentieri <olivierargentieri@gmail.com>
2022-06-01 14:19:37 +0200

    remove react-code-blocks and update CodeSnippet component

--- src/components/codeSnippetComponent/index.tsx
+++ src/components/codeSnippetComponent/index.tsx
@@ -1,23 +1,35 @@
-import React, { useState } from 'react';
-import { CodeBlock, vs2015  } from "react-code-blocks";
+import React, { useState } from "react";
+import Highlight, { defaultProps } from "prism-react-renderer";
+import dracula from "prism-react-renderer/themes/dracula";
 import axios from "axios";
 
 export default function CodeSnippet(props) {
   const [data, setData] = useState("");
-  axios.get(props.rawUrl)
-  .then(data => {
-    setData(data.data)
-  })
+  axios.get(props.rawUrl).then((data) => {
+    setData(data.data);
+  });
 
   return (
     <div>
-      <code style={{padding: "0px 10px"}}> {props.fileName}</code>
-      <CodeBlock
-      text={data}
-      language={props.langage}
-      theme={vs2015}
-      wrapLines
-      />
+      <code style={{ padding: "0px 10px" }}> {props.fileName}</code>
+      <Highlight
+        {...defaultProps}
+        theme={dracula}
+        code={data}
+        language={props.langage}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
     </div>
   );
 }
